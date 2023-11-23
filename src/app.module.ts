bd6b23bef910fc4ed694238be17eaefba4de7dac
Wilber Salcedo <Developer1@GRUPOREAL.COM>
2023-11-23 16:25:38 -0400

    preparar mongodb y envs

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { join } from 'path'; // en Node
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+    
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'), 
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb'
+    }),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule,
+
+  ],
+})
+export class AppModule {}
